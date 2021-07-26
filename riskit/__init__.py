4cce70baf91d3e378c27727b379da041da694f40
Slamwise <58749535+Slamwise@users.noreply.github.com>
2021-07-25 20:49:24 -0400

    Update __init__.py

--- riskit/__init__.py
+++ riskit/__init__.py
@@ -1,1 +1,18 @@
-from call import *
+from riskit.riskit import call
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+
+__all__ = ['call']
+
+try:
+    _dist = get_distribution('riskit')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'riskit')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
